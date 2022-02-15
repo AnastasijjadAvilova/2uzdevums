7f99c0319a8e445db2d4ffe3eb300071a4438877
Anastasia Avilova <77328272+AnastasijjadAvilova@users.noreply.github.com>
2022-02-15 12:11:23 +0000

    1uzdevums

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-summa = 0
-x = int(input("Введите количество нечетных чисел для суммы: "))
-for y in range(1,x+1):
- print( (2*y - 1))
- summa += (2*y - 1) 
-print("Cумма равна: " + str(summa))
+for x in range(6):
+ print(3 ** (x+1))
